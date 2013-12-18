3cb2997b9e270e5c1c39b2ae79b47ec59f492ea6
Savvin Sergei <sav1ch@hotmail.com>
2013-12-19 00:40:44 +0400

    Database: added User class

---
+++ src/main/java/ru/vsu/cs/zombie/server/logic/db/User.java
@@ -0,0 +1,34 @@
+package ru.vsu.cs.zombie.server.logic.db;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "users")
+public class User {
+
+    @DatabaseField(generatedId = true)
+    private int id;
+
+    @DatabaseField(canBeNull = false)
+    private String name;
+
+    @DatabaseField(canBeNull = false)
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
