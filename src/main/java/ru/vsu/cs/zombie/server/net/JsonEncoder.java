9038879f286a00e3cc3d7f57599ac6a23d119866
Boldinova Natalya <boldinova_natalya@mail.ru>
2014-02-25 23:00:09 +0400

    Net: Added web sockets

--- src/main/java/ru/vsu/cs/zombie/server/net/JsonEncoder.java
+++ src/main/java/ru/vsu/cs/zombie/server/net/JsonEncoder.java
@@ -2,6 +2,7 @@ package ru.vsu.cs.zombie.server.net;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
 import ru.vsu.cs.zombie.server.command.Command;
@@ -12,8 +13,7 @@ public class JsonEncoder extends MessageToMessageEncoder<Command> {
     @Override
     protected void encode(ChannelHandlerContext channelHandlerContext, Command command, List<Object> objects) throws Exception {
        // ObjectMapper mapper = new ObjectMapper();
-        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String json = ow.writeValueAsString(command);
-        objects.add(json);
+        ObjectWriter ow = new ObjectMapper().writer();
+        objects.add(new TextWebSocketFrame(ow.writeValueAsString(command)));
     }
 }
