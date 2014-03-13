a5e9b500d9730265a6802d987b24b4f1a873692d
Boldinova Natalya <boldinova_natalya@mail.ru>
2014-03-13 18:58:52 +0400

    Net: added json parsing

--- src/main/java/ru/vsu/cs/zombie/server/net/JsonDecoder.java
+++ src/main/java/ru/vsu/cs/zombie/server/net/JsonDecoder.java
@@ -3,6 +3,7 @@ package ru.vsu.cs.zombie.server.net;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import ru.vsu.cs.zombie.server.command.Command;
 
@@ -12,6 +13,7 @@ public class JsonDecoder extends MessageToMessageDecoder<TextWebSocketFrame> {
     @Override
     protected void decode(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame stringJson, List<Object> objects) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
-        objects.add(mapper.readValue(stringJson.text(), Command.class));
+        JsonNode rootNode = mapper.readTree(stringJson.text());
+        objects.add(mapper.readValue(stringJson.text(), Command.getClassByName(rootNode.path("name").getTextValue())));
     }
 }
