c203713c61ea045d63cfb7dba0ecc89eac3ccc8a
Boldinova Natalya <boldinova_natalya@mail.ru>
2014-03-13 19:21:40 +0400

    net: fixed connection error

--- src/main/java/ru/vsu/cs/zombie/server/net/JsonDecoder.java
+++ src/main/java/ru/vsu/cs/zombie/server/net/JsonDecoder.java
@@ -4,6 +4,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToMessageDecoder;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.ObjectMapper;
 import ru.vsu.cs.zombie.server.command.Command;
 
@@ -12,8 +13,13 @@ import java.util.List;
 public class JsonDecoder extends MessageToMessageDecoder<TextWebSocketFrame> {
     @Override
     protected void decode(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame stringJson, List<Object> objects) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode rootNode = mapper.readTree(stringJson.text());
-        objects.add(mapper.readValue(stringJson.text(), Command.getClassByName(rootNode.path("name").getTextValue())));
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(stringJson.text());
+            objects.add(mapper.readValue(stringJson.text(), Command.getClassByName(rootNode.path("name").getTextValue())));
+        } catch (JsonParseException e) {
+            e.printStackTrace();
+        }
     }
 }
