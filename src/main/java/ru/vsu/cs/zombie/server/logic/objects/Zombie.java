8c967b0185f10f710c4d1cabe3c867107635c7a3
Sergey Savvin <sav1ch@hotmail.com>
2014-04-22 22:04:37 +0400

    Added zombie action

--- src/main/java/ru/vsu/cs/zombie/server/logic/objects/Zombie.java
+++ src/main/java/ru/vsu/cs/zombie/server/logic/objects/Zombie.java
@@ -8,4 +8,10 @@ public class Zombie extends  Character {
     public Zombie(Point position, Island island) {
         super(position, island, new Teeth());
     }
+
+    public void action() {
+        int x = (int)Math.round(Math.random() * Island.WIDTH);
+        int y = (int)Math.round(Math.random() * Island.HEIGHT);
+        move(x, y);
+    }
 }
