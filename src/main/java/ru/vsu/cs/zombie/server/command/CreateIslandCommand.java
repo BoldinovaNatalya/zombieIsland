86249716a8a7b29274054bec0b3668c2d901299e
Sergey Savvin <sav1ch@hotmail.com>
2014-04-22 22:28:01 +0400

    Fixed CreateIslandCommand

--- src/main/java/ru/vsu/cs/zombie/server/command/CreateIslandCommand.java
+++ src/main/java/ru/vsu/cs/zombie/server/command/CreateIslandCommand.java
@@ -13,11 +13,9 @@ public class CreateIslandCommand extends Command {
             if (session.getIsland() == null) {
                 Island island = Island.createIsland(players);
                 result = createResponse();
-                if (players == 1) {
-                    Command tmp = Command.createResponse(Command.JOIN_ISLAND, id, session);
-                    tmp.parameters.put("island_id", Island.getIslands().indexOf(island));
-                    session.process(tmp);
-                }
+                Command tmp = Command.createResponse(Command.JOIN_ISLAND, id, session);
+                tmp.parameters.put("island_id", Island.getIslands().indexOf(island));
+                session.process(tmp);
             } else {
                 result = new ErrorCommand("This user already has an island", id);
             }
